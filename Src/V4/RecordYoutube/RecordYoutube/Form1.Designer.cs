c239da91c844de0fb9b45c98924bd2953e946f4b
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-06-15 06:27:55 +0200

    Add a version

---
+++ Src/V4/RecordYoutube/RecordYoutube/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace RecordYoutube
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(508, 344);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(119, 350);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(317, 350);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 385);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "RecordYoutube";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Src/V4/RecordYoutube/RecordYoutube/Form1.cs
@@ -0,0 +1,109 @@
+﻿using Accord.Video.FFMPEG;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ValueStateChanged;
+
+namespace RecordYoutube
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        [DllImport("advapi32.dll")]
+        private static extern bool LogonUser(string lpszUsername, string lpszDomain, string lpszPassword, int dwLogonType, int dwLogonProvider, out IntPtr phToken);
+        [DllImport("winmm.dll", EntryPoint = "timeBeginPeriod")]
+        public static extern uint TimeBeginPeriod(uint ms);
+        [DllImport("winmm.dll", EntryPoint = "timeEndPeriod")]
+        public static extern uint TimeEndPeriod(uint ms);
+        [DllImport("ntdll.dll", EntryPoint = "NtSetTimerResolution")]
+        public static extern void NtSetTimerResolution(uint DesiredResolution, bool SetResolution, ref uint CurrentResolution);
+        [DllImport("user32.dll")]
+        public static extern bool GetAsyncKeyState(System.Windows.Forms.Keys vKey);
+        private static Timer timer1;
+        private static VideoFileWriter vf;
+        private static Bitmap bp;
+        private static Graphics gr;
+        private static string outputvideo, outputaudio, output, outputvideotemp, outputaudiotemp, outputtemp, videodelay, ss;
+        private static bool capturing;
+        private static NAudio.Wave.MediaFoundationReader audioFileReader;
+        private static NAudio.Wave.IWavePlayer waveOutDevice;
+        private static CSCore.SoundIn.WasapiCapture captureaudio;
+        private static CSCore.Codecs.WAV.WaveWriter wavewriter;
+        private static StreamReader errorreadermerge;
+        private static Process processmerge;
+        public static System.Threading.ThreadStart threadstart;
+        public static System.Threading.Thread thread;
+        private static int width = 800, height = 600;
+        public static uint CurrentResolution = 0;
+        private static valuechanged ValueChanged = new valuechanged();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1 = new Timer();
+            timer1.Interval = 20;
+            timer1.Enabled = true;
+            timer1.Tick += timer1_Tick;
+            vf = new VideoFileWriter();
+            vf.Open("output.avi", 800, 600, 25, VideoCodec.MPEG4, 1000000);
+            timer1.Start();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            vf.Close();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            bp = new Bitmap(800, 600);
+            gr = Graphics.FromImage(bp);
+            gr.CopyFromScreen(0, 0, 0, 0, new Size(bp.Width, bp.Height));
+            pictureBox1.Image = bp;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            vf.WriteVideoFrame(bp);
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            OnKeyDown(e.KeyData);
+        }
+        private static void OnKeyDown(System.Windows.Forms.Keys keyData)
+        {
+            if (keyData == System.Windows.Forms.Keys.F1)
+            {
+                const string message = "• Author: Michaël André Franiatte.\n\r\n\r• Contact: michael.franiatte@gmail.com.\n\r\n\r• Publisher: https://github.com/michaelandrefraniatte.\n\r\n\r• Copyrights: All rights reserved, no permissions granted.\n\r\n\r• License: Not open source, not free of charge to use.";
+                const string caption = "About";
+                System.Windows.Forms.MessageBox.Show(message, caption, System.Windows.Forms.MessageBoxButtons.OK, System.Windows.Forms.MessageBoxIcon.Information);
+            }
+        }
+    }
+}
+namespace ValueStateChanged
+{
+    public class valuechanged
+    {
+        public static bool[] _valuechanged = { false, false, false };
+        public static bool[] _ValueChanged = { false, false, false };
+        public bool this[int index]
+        {
+            get { return _ValueChanged[index]; }
+            set
+            {
+                if (_valuechanged[index] != value)
+                    _ValueChanged[index] = true;
+                else
+                    _ValueChanged[index] = false;
+                _valuechanged[index] = value;
+            }
+        }
+    }
+}
